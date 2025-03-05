1b4a76e8106a80f89dd43f37f7544f1dd9c0c9d3
chicacode <gerahernan.al@gmail.com>
2025-03-04 20:01:55 -0800

    feat: added load budget fn

--- budget_management.py
+++ budget_management.py
@@ -1,3 +1,5 @@
+import os.path
+
 import pandas as pd
 
 BUDGET_FILE = "budget.csv"
@@ -8,3 +10,11 @@ def set_budget(amount):
     data = pd.DataFrame({"Budget" : [amount]})
     data.to_csv(BUDGET_FILE, index=False)
     print(f"✅ Budget set to ${amount}")
+
+def load_budget():
+    """Load budget from CSV file or return default if is not set a new one"""
+    if os.path.exists(BUDGET_FILE):
+        data = pd.read_csv(BUDGET_FILE)
+        return data.loc[0, "Budget"]
+    # Default budget
+    return 3000
