6db579557976e93a45a0bf0c5a061ebaa75a529c
chicacode <gerahernan.al@gmail.com>
2025-03-04 19:23:26 -0800

    feat: added csv file, visualization function w matplotlib

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello personal tracker feature b')
+from visualization import plot_spending_by_category
+import pandas as pd
+
+file_path = "sampledata.csv"
+
+def load_data(file_path):
+    """Load transactions from a CSV file."""
+    return pd.read_csv(file_path)
+
+data = load_data(file_path)
+
+print(data)
+
+plot_spending_by_category(data)
