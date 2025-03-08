a4e9ba2446fbe2039ff3758c6e4fd8d621935c02
chicacode <gerahernan.al@gmail.com>
2025-03-07 19:22:48 -0800

    fix: latest bugs

--- menu.py
+++ menu.py
@@ -8,7 +8,7 @@ def main_menu():
             4. Edit a Transaction
             5. Delete a Transaction
             6. Analyze Spending by Category
-            7. Calculate Average Monthly Spending
+            7. Calculate Total Monthly Spending
             8. Show Top Spending Category
             9. Visualize spending
             10. Set Budget
--- visualization.py
+++ visualization.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 
 def plot_spending_by_category(data):
-    """Generate a bar chart of spending by category."""
     spending = data.groupby('Category')['Amount'].sum()
     spending.plot(kind='bar', title="Spending by Category", ylabel="Amount ($)")
     plt.show()
