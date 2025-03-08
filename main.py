581ca63517b69f49e0dcbeb388206ab3caa108f6
chicacode <gerahernan.al@gmail.com>
2025-03-07 17:03:58 -0800

    feat: remove index

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
         # Choice verification
         main_menu()
         user_choice = input("Your Choice : ")
-        if user_choice in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']:
+        if user_choice in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']:
 
             # Main choices
             if user_choice == '0':
